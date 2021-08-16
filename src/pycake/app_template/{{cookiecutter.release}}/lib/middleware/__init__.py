8d1e193b168d5be11b8736963a7ff7135f98c218
chuter <liulong@kanzhun.com>
2021-08-16 22:18:00 +0800

    feat: 升级模型采样的实现基于sanic

--- src/pycake/app_template/{{cookiecutter.release}}/lib/middleware/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-from __future__ import absolute_import
-
-from .model_sample import build_model_sample_middleware
-
-__all__ = [
-    'build_model_sample_middleware'
-]
